366738e78f3030d8e756ceffc85c9691e9085c15
kristian <kristiantarnaj@gmail.com>
2023-02-16 15:43:17 +0100

    feat(web): nav for mobile

--- web/src/lib/Header.svelte
+++ web/src/lib/Header.svelte
@@ -1,13 +1,19 @@
-<script>
+<script lang="ts">
   import {page} from '$app/stores';
+  import { fly } from 'svelte/transition';
+
+
+  let menu: boolean = false;
+  let y
 </script>
-<div class="grid">
+
+<div class="grid desktop">
   <div class="col-12 flex jc-center">
     <img src="/logo.png" alt="" class="logo">
   </div>
 </div>
 
-<header>
+<header class="nav">
   <div class="grid jc-center h-full">
     <nav class="flex jc-center">
       <a href="/" class:active={$page.url.pathname === '/'}>Početna</a>
@@ -20,18 +26,53 @@
   </div>
 </header>
 
+<div class="nav-mobile" class:shadow={y > 0}>
+  <img src="/logo.png" alt="" class="logo">
+
+  <button on:click={() => menu = !menu}>
+    <img src={menu ? '/icons/close.svg' : '/icons/menu.svg'} alt="" height="40">
+  </button>
+</div>
+
+{#if menu}
+  <div class="menu" in:fly="{{ y: -200, duration: 200 }}">
+    <a href="/" class:active={$page.url.pathname === '/'} on:click={() => menu = false}>Početna</a>
+    <a href="/o-nama" class:active={$page.url.pathname === '/o-nama'} on:click={() => menu = false}>O nama</a>
+    <a href="/certifikacije" class:active={$page.url.pathname === '/certifikacije'} on:click={() => menu = false}>Certifikacije</a>
+    <a href="/zakonske-regulative" class:active={$page.url.pathname === '/zakonske-regulative'} on:click={() => menu = false}>Regulative</a>
+    <a href="/obrasci" class:active={$page.url.pathname === '/obrasci'} on:click={() => menu = false}>Obrasci</a>
+    <a href="/kontakt" class:active={$page.url.pathname === '/kontakt'} on:click={() => menu = false}>Kontakt</a>
+  </div>
+{/if}
+
+<svelte:window bind:scrollY={y} />
+
 <style lang="scss">
   .logo {
     height: 144px;
+
+    @media (max-width: 600px) {
+      height: 100px;
+    }
   }
 
-  header {
+  .desktop {
+    @media (max-width: 600px) {
+      display: none;
+    }
+  }
+
+  .nav {
     z-index: 5;
     position: sticky;
     top: 0;
     background-color: var(--primary);
     height: 2.5rem;
     width: 100%;
+
+    @media (max-width: 600px) {
+      display: none;
+    }
 
     nav {
       height: 100%;
@@ -54,6 +95,57 @@
           color: var(--primary);
           opacity: 1;
         }
+      }
+    }
+  }
+
+  .nav-mobile {
+    z-index: 5;
+    position: sticky;
+    top: 0;
+    display: none;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--primary-c);
+    padding: 1rem;
+
+    @media (max-width: 600px) {
+      display: flex;
+    }
+
+    &.shadow {
+      box-shadow: 0 3px 6px rgba(0,0,0,.12);
+    }
+
+    button {
+      padding: 1rem;
+      background-color: var(--primary-l);
+      border: none;
+      cursor: pointer;
+      border-radius: 1rem;
+    }
+  }
+
+  .menu {
+    z-index: 4;
+    position: fixed;
+    top: 124px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    height: calc(100vh - 124px);
+    padding: 1rem;
+    background-color: white;
+
+    a {
+      font-size: 3rem;
+      transition: .35s;
+
+      &.active {
+        transform: translateX(2rem);
+        color: var(--primary);
+        text-decoration: underline;
       }
     }
   }
